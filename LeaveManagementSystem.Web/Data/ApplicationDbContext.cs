4881e1732440d2725889a168efa08dec0d34d034
siphesihle <madondosphesihles@gmail.com>
2025-05-13 11:12:01 +0200

    Completed Leave Management Module.

--- LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
+++ LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
@@ -3,11 +3,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagementSystem.Web.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext // inheritence from the IdentityDbContext , 
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+
+        //(Constructor) get the info (options) when the application starts up, so you can use it here, and also pass it to base clase (IdentityDbContext)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
             : base(options)
         {
         }
+
+        //the datatype of LeaveType which represent a set/collection of records
+        // It is recommended to make the entity/table named plural
+        public DbSet<LeaveType> LeaveTypes { get; set; }
     }
+
 }
---
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementSystem.Web.Data
+{
+    // Entity - Data Model.
+    public class LeaveType
+    {
+        // EF Core will automatically know this is a primary key // best practice.
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(150)")]
+        public string Name { get; set; }
+
+        [Precision(18, 2)]
+        public decimal NumberOfDays { get; set; }
+    }
+
+}
